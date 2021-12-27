f392b2e5b8fa1a176bcdf69f4aa7a92c05d30ac4
Cenk Kılıç <cenk@kilic.dev>
2021-12-27 15:18:55 +0100

    feat: update utils

--- utils/tasks.go
+++ utils/tasks.go
@@ -204,13 +204,3 @@ func HandleOutputStreamReader(reader *bufio.Reader, context TaskMetadata, level 
 		log.Logln(level, str)
 	}
 }
-
-func DeleteEmptyStringsFromSlice(s []string) []string {
-	var r []string
-	for _, str := range s {
-		if str != "" {
-			r = append(r, str)
-		}
-	}
-	return r
-}

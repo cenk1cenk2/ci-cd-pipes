a025931b272e6ff71c210794f7c400efad0ed8db
Cenk Kılıç <cenk@kilic.dev>
2022-02-07 15:07:36 +0100

    update logger

--- utils/logger.go
+++ utils/logger.go
@@ -18,7 +18,7 @@ func InitiateLogger(level logrus.Level) {
 	Log.SetFormatter(&nested.Formatter{
 		FieldsOrder: []string{"context"},
 		// TimestampFormat:  "[20060102-15:04:05]",
-		TimestampFormat:  "",
+		TimestampFormat:  "[15:04:05]",
 		HideKeys:         true,
 		NoColors:         false,
 		NoFieldsColors:   false,
